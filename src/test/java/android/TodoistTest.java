b7dd95bf412b6a27007bbd700fc86cbac857f7b7
FkkfRf <Fkkf.Rf@gmail.com>
2023-01-23 01:39:55 +0300

    Update loginPassword

--- src/test/java/android/TodoistTest.java
+++ src/test/java/android/TodoistTest.java
@@ -5,20 +5,16 @@ import config.MobileConfig;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Test;
 
-
 public class TodoistTest extends BaseTest {
     LoginPage loginPage = new LoginPage();
     MobileConfig mobileConfig = ConfigFactory.create(MobileConfig.class);
 
     String loginEmail = mobileConfig.Email();
-    String logimPassword = mobileConfig.Passord();
+    String loginPassword = mobileConfig.Passord();
 
     @Test
     void addProgectTest() {
-        loginPage.loginByEmail(loginEmail,logimPassword);
-
-
-
+        loginPage.loginByEmail(loginEmail, loginPassword);
 
 
     }
